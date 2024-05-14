5453872df21ec26e20c12f382ab878ee8f0ed6c0
AliShams2002 <alishamsaldin01@gmail.com>
2024-05-14 04:09:20 -0700

    v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#0c2444',
+        'pink': '#0437F2',
+        'aqua': '#f1f1f1'
+      },
+
+      screens: {
+        's': '500px'
+      }
+    },
   },
   plugins: [],
 }
